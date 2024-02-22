141fd2e52f31a834dbc2bdfb8cc25860a0ce24f3
Kaue Christian <141335889+KaueChristian@users.noreply.github.com>
2024-02-22 19:01:09 -0300

    adjust comments

--- router/routes.go
+++ router/routes.go
@@ -3,17 +3,12 @@ package router
 import (
 	"github.com/KaueChristian/Goportunitties/handler"
 	"github.com/gin-gonic/gin"
-	docs "github.com/KaueChristian/Goportunitties/docs"
-   swaggerfiles "github.com/swaggo/files"
-   ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 // initializeRoutes initializes routes for the provided Gin router.
 func initializeRoutes(router *gin.Engine) {
 	// Initialize the handler
 	handler.InitializeHandler()
-	basePath := "/api/v1"
-	docs.SwaggerInfo.BasePath = basePath 
 	// Grouping routes under /api/v1
 	v1 := router.Group("/api/v1")
 	{
@@ -24,7 +19,4 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/opening", handler.UpdateOpeningHandler) // Update a specific opening
 		v1.GET("/openings", handler.ListOpeningHandler)  // List all openings
 	}
-
-	router.GET("/swagger/*any",ginSwagger.WrapHandler(swaggerfiles.Handler))
-
 }
